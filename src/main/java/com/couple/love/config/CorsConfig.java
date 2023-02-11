c81f08c46c1cb5157a33884f14d03ae4d1a7e9d5
sunghyeon.jo <sunghyeon.jo@navercorp.com>
2023-02-11 21:34:48 +0900

    Feat : 안 쓰는 코드 제거(#6)

--- src/main/java/com/couple/love/config/CorsConfig.java
+++
@@ -1,4 +0,0 @@
-package com.couple.love.config;
-
-public class CorsConfig {
-}
--- src/main/java/com/couple/love/config/SecurityConfig.java
+++ src/main/java/com/couple/love/config/SecurityConfig.java
@@ -1,19 +1,15 @@
 package com.couple.love.config;
 
 import com.couple.love.auth.*;
-import com.couple.love.domain.member.api.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,12 +18,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebSecurity
 public class SecurityConfig implements WebMvcConfigurer {
 
-    private final TokenService tokenService;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private final CustomCorsFilter customCorsFilter;
 
     private final PermissionInterceptor permissionInterceptor;
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -49,23 +43,7 @@ public class SecurityConfig implements WebMvcConfigurer {
                         request.requestMatchers("/**").permitAll()
                 )
                 .build();
-//
-//                .csrf().disable() // token을 사용하는 방식이기 때문에 csrf disable
-//                .addFilter(customCorsFilter.corsFilter())
-//                .addFilterBefore(new JwtFilter(tokenService), UsernamePasswordAuthenticationFilter.class)
-//                .exceptionHandling()
-//                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-//                .accessDeniedHandler(jwtAccessDeniedHandler)
-//                .and()
-//                .authorizeHttpRequests((request) ->
-//                        request.requestMatchers("/**").hasAnyRole("USER", "MANAGER", "ADMIN")
-//                )
-//                .build();
-    }
 
-//
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
-//    }
+    }
 }
+
