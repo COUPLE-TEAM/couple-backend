d137611df8df2b15552946303fa790d9f6bf0244
shinkyuchul <kc960923@naver.com>
2023-02-14 17:12:13 +0900

    Hotfix : 사용자 정의 레포지토리 수정

--- src/main/java/com/couple/love/domain/memo/repository/MemoRespository.java
+++ src/main/java/com/couple/love/domain/memo/repository/MemoRespository.java
@@ -2,8 +2,6 @@ package com.couple.love.domain.memo.repository;
 
 import com.couple.love.domain.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +9,4 @@ import java.util.List;
 @Repository
 public interface MemoRespository extends JpaRepository<Memo,Long>, MemoRepositoryCustom{
 
-    List<Memo> findByMemberId(Long memberId);
 }
